b8511bcfad05741723d1ecd3230965ab868b95b9
Julian Montufar <julamontufar@misena.edu.co>
2024-05-13 16:54:49 -0500

    Se agrega debounce para la busqueda

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -12,16 +12,19 @@ import { Country } from '../../interfaces/country';
 export class ByCapitalPageComponent {
 
   public countries: Country[] = [];
+  public isLoading: boolean = false;
 
   constructor(private countriesService: CountryService) {
 
   }
 
   searchByCapital(term: string): void {
+    this.isLoading = true;
 
     this.countriesService.searchCapital(term)
-      .subscribe(countries =>
-        this.countries = countries
-      );
+      .subscribe(countries => {
+        this.countries = countries;
+        this.isLoading = false;
+      });
   }
 }
